7b7ab2fd647586808713b191410ca11e24a56a03
doga <doga@dogas-MBP.local>
2019-06-05 20:41:40 +0200

    feat(api): sanitize genders and group age groups after fetch

--- api/survey.js
+++ api/survey.js
@@ -1,4 +1,5 @@
 import fetch from 'isomorphic-unfetch';
+import { compose, map, assoc, includes, __ } from 'ramda';
 
 const API_URL = "http://localhost:3001/results";
 
@@ -16,10 +17,48 @@ const Survey = {
       const query = limit ? `${API_URL}?_limit=${limit}` : `${API_URL}`;
       fetch(query)
         .then(response => response.json())
-        .then(json => resolve(json))
+        .then(json => resolve(compose(
+          this.groupAges,
+          this.sanitizeGender,
+        )(json)))
         .catch(error => reject(error));
     });
   },
+  
+  /**
+   * Distribute age into groups.
+   */
+  groupAges: function(surverResults) {
+    const assocAge = assoc('Age');
+    return map(result => {
+      const age = result['Age'];
+      if (age < 24) return assocAge('0-23', result);
+      if (age < 36) return assocAge('24-35', result);
+      if (age < 45) return assocAge('36-44', result);
+      if (age < 61) return assocAge('46-60', result);
+      return assocAge('over 61');
+    })(surverResults);
+  },
+
+  /**
+   * Group common genders.
+   */
+  sanitizeGender: function(surverResults) {
+    const assocGender = assoc('Gender');
+    const belongsMaleGender = includes(__, ['male', 'm', 'cis male']); 
+    const belongsFemaleGender = includes(__, ['female', 'f', 'cis female']);
+    return map(result => {
+      const gender = result['Gender'];
+      const genderLowerCase = gender.toLowerCase();
+      if (belongsMaleGender(genderLowerCase)) {
+        return assocGender('Male', result);
+      }
+      if (belongsFemaleGender(genderLowerCase)) {
+        return assocGender('Female', result);
+      }
+      return assocGender('Other', result);
+    })(surverResults);
+  }
 };
 
 export default Survey;
